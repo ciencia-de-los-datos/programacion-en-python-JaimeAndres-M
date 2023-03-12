10fccfbdeaf29063a02687b5ff764a7f669c253e
JaimeAndres-M <jaamolinaco@unal.edu.co>
2023-03-12 11:40:46 -0500

    preguntas 7 a 12 v1

--- preguntas.py
+++ preguntas.py
@@ -230,7 +230,7 @@ def pregunta_06():
     for i in aL:
         a = (i, min(dc[i]),max(dc[i]))
         lista_final.append(a)
-        
+
     return lista_final
 
 
@@ -255,7 +255,22 @@ def pregunta_07():
     ]
 
     """
-    return
+
+    ltr = [i.split("\t")[0] for i in lst]
+    num = [i.split("\t")[1] for i in lst]
+    t = []
+    [t.append(int(i)) for i in num if i not in t]
+    numx = []
+    [numx.append(int(i)) for i in num if i not in numx]
+    numx.sort()
+    dc = {k:[] for k in numx}
+    c = 0
+    for i in t:
+        int(i)
+        dc[i].append(ltr[c])
+        c += 1
+    dev = list(dc.items())
+    return dev
 
 
 def pregunta_08():
@@ -280,7 +295,28 @@ def pregunta_08():
     ]
 
     """
-    return
+    ltr = [i.split("\t")[0] for i in lst]
+    num = [i.split("\t")[1] for i in lst]
+    h = []
+    [h.append(int(i)) for i in num if i not in h]
+    numx = []
+    [numx.append(i) for i in h if i not in numx]
+    numx.sort()
+    lo = []
+    [lo.append(i) for i in numx if i not in lo]
+    dc = {k:[] for k in lo}
+    c = 0
+    for i in h:
+        dc[i].append(ltr[c])
+        c += 1
+    for i in lo:
+        j = list(dc[i])
+        u = []
+        [u.append(p) for p in j if p not in u]
+        u.sort()
+        dc[i] = u
+    dev = list(dc.items())
+    return dev
 
 
 def pregunta_09():
@@ -303,7 +339,22 @@ def pregunta_09():
     }
 
     """
-    return
+    rg = [i.split("\t")[4] for i in lst]
+    rg = [i.split(",") for i in rg]
+    rgf = []
+    [rgf.extend(i) for i in rg]
+    rg = [i.split(":") for i in rgf]
+    lista_codigos = []
+    for i in rg:
+        lista_codigos.append(i[0])
+    R = []
+    [R.append(i) for i in lista_codigos if i not in R]
+    R.sort()
+    ds = {key:0 for key in R}
+    for i in R:
+        a = lista_codigos.count(i)
+        ds[i] = a
+    return(ds)
 
 
 def pregunta_10():
@@ -322,9 +373,19 @@ def pregunta_10():
         ("E", 3, 3),
     ]
 
-
     """
-    return
+    ltr = [i.split("\t")[0] for i in lst] 
+    rg = [i.split("\t")[3] for i in lst]
+    rg = [i.replace(",","") for i in rg] 
+    dis = [i.split("\t")[4] for i in lst]
+    dis = [i.split(",") for i in dis] 
+    c = 0
+    listaF = []
+    for i in ltr:
+        a = (i,(len(rg[c])),(len(dis[c])))
+        listaF.append(a)
+        c += 1
+    return(listaF)
 
 
 def pregunta_11():
@@ -345,7 +406,24 @@ def pregunta_11():
 
 
     """
-    return
+    cd = [i.split("\t")[3] for i in lst]
+    cd = [i.split(",") for i in cd]
+    nm = [i.split("\t")[1] for i in lst]
+    ids = []
+    for i in cd:
+        ids.extend(i)
+    idd = []
+    [idd.append(i) for i in ids if i not in idd]
+    idd.sort()
+    ids = {key: 0 for key in idd}
+    c = 0
+    for i in cd:
+        a = nm[c]
+        for j in i:
+            suma_aux = int(a) + int(ids.get(j))
+            ids[j] = suma_aux
+        c += 1
+    return ids
 
 
 def pregunta_12():
@@ -363,4 +441,22 @@ def pregunta_12():
     }
 
     """
-    return
+
+    ID = [i.split("\t")[0] for i in lst]
+    ct = [i.split("\t")[4] for i in lst]
+    IDi = []
+    [IDi.append(i) for i in ID if i not in IDi]
+    IDi.sort()
+    dc = {key:0 for key in IDi}
+    pos = 0
+    for i in ct:
+        u = i.split(",")
+        c1 = 0
+        for j in u:
+            h = j.split(":")
+            c1 = c1 + int(h[1])    
+        ky = ID[pos]
+        auxiliar_de_suma = c1 + int(dc.get(ky))
+        dc[ky] = auxiliar_de_suma
+        pos = pos + 1
+    return dc
